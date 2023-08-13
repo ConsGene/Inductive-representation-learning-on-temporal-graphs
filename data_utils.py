480d09016b5e975bb334fed353ccc127b294b4ae
tejaswi3dabascd/Users/tejaswidabas/Desktop/untitledfolder/ <tdabas1922@email.com>
2023-08-13 13:01:30 +0530

    refactored and fixed the old commit and resolved the comments

--- data_utils.py
+++ data_utils.py
@@ -5,36 +5,35 @@ import argparse
 import torch
 import pandas as pd
 import numpy as np
-#import numba
+# import numba
 from tqdm import tqdm
 
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_absolute_error
 from sklearn import preprocessing
 
+
 class Data:
-  def __init__(self, sources, destinations, dest_categories, timestamps, edge_idxs, raw_labels, labels):
-    self.sources = sources
-    self.destinations = destinations
-    self.dest_categories = dest_categories
-    self.timestamps = timestamps
-    self.edge_idxs = edge_idxs
-    self.raw_labels = raw_labels
-    self.labels = labels
+    def __init__(self, sources, destinations, dest_categories, timestamps, edge_idxs, raw_labels, labels):
+        self.sources = sources
+        self.destinations = destinations
+        self.dest_categories = dest_categories
+        self.timestamps = timestamps
+        self.edge_idxs = edge_idxs
+        self.raw_labels = raw_labels
+        self.labels = labels
 
 
-
-def get_data(dataset_name, scale_label, device):
-    ### Load data and train val test split
+def get_data(dataset_name, scale_label, device, num_classes=10):
+    # Load data and train val test split
     graph_df = pd.read_csv('./processed/ml_{}.csv'.format(dataset_name))
     edge_features = np.load('./processed/ml_{}.npy'.format(dataset_name))
-    node_features = np.load('./processed/ml_{}_node.npy'.format(dataset_name)) 
+    node_features = np.load('./processed/ml_{}_node.npy'.format(dataset_name))
 
     val_time, test_time = list(np.quantile(graph_df.ts, [0.70, 0.85]))
 
     ts_l = graph_df.ts.values
     valid_train_flag = (ts_l <= val_time)
-
 
     train_df = graph_df[valid_train_flag]
     train_i = train_df.i.unique()
@@ -53,24 +52,24 @@ def get_data(dataset_name, scale_label, device):
     scaleUtil = ScaleUtil(scale_label, device, num_classes=num_classes)
     label_l, raw_label_l = scaleUtil.transform_df(graph_df, valid_train_flag)
 
-    full_data = Data(sources=src_l, 
-            destinations=dst_l, 
-            dest_categories=cat_l, 
-            timestamps=ts_l, 
-            edge_idxs=e_idx_l, 
-            raw_labels=raw_label_l, 
-            labels=label_l)
+    full_data = Data(sources=src_l,
+                     destinations=dst_l,
+                     dest_categories=cat_l,
+                     timestamps=ts_l,
+                     edge_idxs=e_idx_l,
+                     raw_labels=raw_label_l,
+                     labels=label_l)
 
     def get_dataset(flag):
-        ds = Data(sources=src_l[flag], 
-            destinations=dst_l[flag], 
-            dest_categories=cat_l[flag], 
-            timestamps=ts_l[flag], 
-            edge_idxs=e_idx_l[flag], 
-            raw_labels=raw_label_l[flag], 
-            labels=label_l[flag])
+        ds = Data(sources=src_l[flag],
+                  destinations=dst_l[flag],
+                  dest_categories=cat_l[flag],
+                  timestamps=ts_l[flag],
+                  edge_idxs=e_idx_l[flag],
+                  raw_labels=raw_label_l[flag],
+                  labels=label_l[flag])
         return ds
-    
+
     train_data = get_dataset(valid_train_flag)
     # select validation and test dataset
     valid_val_flag = (ts_l <= test_time) * (ts_l > val_time)
@@ -87,7 +86,7 @@ class ScaleUtil:
         self.num_classes = num_classes
 
         if scale_label == 'MinMax':
-            self.sscaler = preprocessing.MinMaxScaler
+            self.scaler = preprocessing.MinMaxScaler
         elif scale_label == 'Quantile':
             self.scaler = preprocessing.QuantileTransformer
         elif scale_label == 'Log':
@@ -100,9 +99,10 @@ class ScaleUtil:
         self.device = device
         self.i2cat = None
         self.scalers_dict = None
-    
+
     def transform_df(self, original_graph_df, valid_train_flag):
         graph_df = original_graph_df.copy()
+
         graph_df['raw_label'] = graph_df.label.copy()
 
         self.i2cat = graph_df.groupby('i').first(
@@ -113,13 +113,14 @@ class ScaleUtil:
                                                graph_df.cat == cat) & valid_train_flag, 'label'].values
             lower = np.quantile(orig_labels, 0.001)
             upper = np.quantile(orig_labels, 0.999)
-            graph_df.loc[(graph_df.cat == cat) & valid_train_flag, 'label'] = np.clip(orig_labels, lower, upper)
-        
+            graph_df.loc[(graph_df.cat == cat) & valid_train_flag,
+            'label'] = np.clip(orig_labels, lower, upper)
+
         if self.scale_label == 'none':
             label_l = graph_df.label.values
         else:
             # train_df['abs_label'] = train_df['label'].abs()
-            
+
             # i_maxes = train_df.groupby('i')['abs_label'].max().reset_index().set_index('i')['abs_label'].to_dict()
             # # normalize labels with the max value from training set
             # for i, max_label in i_maxes.items():
@@ -132,7 +133,6 @@ class ScaleUtil:
             #         scale = torch.tensor([i_maxes[dst] for dst in dst_l_cut], dtype=float, device=device)
             #     return preds * scale, labels * scale
             train_df = graph_df[valid_train_flag]
-
 
         self.scalers_dict = {}
         if self.scale_label == 'Cbrt':
@@ -185,17 +185,18 @@ class ScaleUtil:
                 raw_label_l = graph_df.raw_label.values
 
         return label_l, raw_label_l
-        
+
     def prepare_transform(self, label_vals):
         if self.scale_label == 'Log':
-            label_vals = np.sign(label_vals) * np.log(np.abs(label_vals)+1)
+            label_vals = np.sign(label_vals) * np.log(np.abs(label_vals) + 1)
         elif self.scale_label == 'Cbrt':
             label_vals = np.cbrt(label_vals)
         return label_vals
 
     def convert_to_raw_label_scale(self, dst_l_cut, preds):
         if (self.i2cat is None) or (self.scalers_dict is None):
-            raise RuntimeError("self.i2cat or self.scalers_dict is None. Run ScaleUtil.transform_df function first")
+            raise RuntimeError(
+                "self.i2cat or self.scalers_dict is None. Run ScaleUtil.transform_df function first")
         raw_preds = []
         for dst, pred in zip(dst_l_cut, preds):
             cat = self.i2cat[dst]
@@ -206,10 +207,13 @@ class ScaleUtil:
                 raw_pred = self.scalers_dict[cat].inverse_transform(
                     [pred.tolist()])  # Convert numpy array to a list
                 if self.scale_label == 'Log':
-                    raw = np.sign(raw) * (np.exp(np.abs(raw))-1)
+                    raw_pred = np.sign(raw_pred) * \
+                               (np.exp(np.abs(raw_pred)) - 1)
+
                 elif self.scale_label == 'Cbrt':
-                    raw = np.power(raw, 3)
-                raw_preds.append(raw[0, 0])
+                    raw_pred = np.power(raw_pred, 3)
+                    # Access the value from the list
+                    raw_preds.append(raw_pred[0][0])
         if isinstance(preds, np.ndarray):
             return np.array(raw_preds)
         else:

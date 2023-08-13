74fe5bbfa9458c5c0947be0e2bc0614c59c84dbd
tejaswi3dabascd/Users/tejaswidabas/Desktop/untitledfolder/ <tdabas1922@email.com>
2023-08-13 13:15:18 +0530

    removed uneccessary changes due to formatting

--- data_utils.py
+++ data_utils.py
@@ -5,27 +5,27 @@ import argparse
 import torch
 import pandas as pd
 import numpy as np
-# import numba
+#import numba
 from tqdm import tqdm
 
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_absolute_error
 from sklearn import preprocessing
 
-
 class Data:
-    def __init__(self, sources, destinations, dest_categories, timestamps, edge_idxs, raw_labels, labels):
-        self.sources = sources
-        self.destinations = destinations
-        self.dest_categories = dest_categories
-        self.timestamps = timestamps
-        self.edge_idxs = edge_idxs
-        self.raw_labels = raw_labels
-        self.labels = labels
+  def __init__(self, sources, destinations, dest_categories, timestamps, edge_idxs, raw_labels, labels):
+    self.sources = sources
+    self.destinations = destinations
+    self.dest_categories = dest_categories
+    self.timestamps = timestamps
+    self.edge_idxs = edge_idxs
+    self.raw_labels = raw_labels
+    self.labels = labels
 
 
-def get_data(dataset_name, scale_label, device, num_classes=10):
-    # Load data and train val test split
+
+def get_data(dataset_name, scale_label, device):
+    ### Load data and train val test split
     graph_df = pd.read_csv('./processed/ml_{}.csv'.format(dataset_name))
     edge_features = np.load('./processed/ml_{}.npy'.format(dataset_name))
     node_features = np.load('./processed/ml_{}_node.npy'.format(dataset_name))
@@ -34,6 +34,7 @@ def get_data(dataset_name, scale_label, device, num_classes=10):
 
     ts_l = graph_df.ts.values
     valid_train_flag = (ts_l <= val_time)
+
 
     train_df = graph_df[valid_train_flag]
     train_i = train_df.i.unique()
@@ -53,21 +54,21 @@ def get_data(dataset_name, scale_label, device, num_classes=10):
     label_l, raw_label_l = scaleUtil.transform_df(graph_df, valid_train_flag)
 
     full_data = Data(sources=src_l,
-                     destinations=dst_l,
-                     dest_categories=cat_l,
-                     timestamps=ts_l,
-                     edge_idxs=e_idx_l,
-                     raw_labels=raw_label_l,
-                     labels=label_l)
+            destinations=dst_l,
+            dest_categories=cat_l,
+            timestamps=ts_l,
+            edge_idxs=e_idx_l,
+            raw_labels=raw_label_l,
+            labels=label_l)
 
     def get_dataset(flag):
         ds = Data(sources=src_l[flag],
-                  destinations=dst_l[flag],
-                  dest_categories=cat_l[flag],
-                  timestamps=ts_l[flag],
-                  edge_idxs=e_idx_l[flag],
-                  raw_labels=raw_label_l[flag],
-                  labels=label_l[flag])
+            destinations=dst_l[flag],
+            dest_categories=cat_l[flag],
+            timestamps=ts_l[flag],
+            edge_idxs=e_idx_l[flag],
+            raw_labels=raw_label_l[flag],
+            labels=label_l[flag])
         return ds
 
     train_data = get_dataset(valid_train_flag)
@@ -102,19 +103,14 @@ class ScaleUtil:
 
     def transform_df(self, original_graph_df, valid_train_flag):
         graph_df = original_graph_df.copy()
-
         graph_df['raw_label'] = graph_df.label.copy()
 
-        self.i2cat = graph_df.groupby('i').first(
-        ).reset_index().set_index('i')['cat'].to_dict()
-
+        self.i2cat = graph_df.groupby('i').first().reset_index().set_index('i')['cat'].to_dict()
         for cat in self.i2cat.values():
-            orig_labels = graph_df.loc[(
-                                               graph_df.cat == cat) & valid_train_flag, 'label'].values
+            orig_labels = graph_df.loc[(graph_df.cat == cat) & valid_train_flag, 'label'].values
             lower = np.quantile(orig_labels, 0.001)
             upper = np.quantile(orig_labels, 0.999)
-            graph_df.loc[(graph_df.cat == cat) & valid_train_flag,
-            'label'] = np.clip(orig_labels, lower, upper)
+            graph_df.loc[(graph_df.cat == cat) & valid_train_flag, 'label'] = np.clip(orig_labels, lower, upper)
 
         if self.scale_label == 'none':
             label_l = graph_df.label.values
@@ -133,6 +129,7 @@ class ScaleUtil:
             #         scale = torch.tensor([i_maxes[dst] for dst in dst_l_cut], dtype=float, device=device)
             #     return preds * scale, labels * scale
             train_df = graph_df[valid_train_flag]
+
 
         self.scalers_dict = {}
         if self.scale_label == 'Cbrt':
@@ -188,15 +185,14 @@ class ScaleUtil:
 
     def prepare_transform(self, label_vals):
         if self.scale_label == 'Log':
-            label_vals = np.sign(label_vals) * np.log(np.abs(label_vals) + 1)
+            label_vals = np.sign(label_vals) * np.log(np.abs(label_vals)+1)
         elif self.scale_label == 'Cbrt':
             label_vals = np.cbrt(label_vals)
         return label_vals
 
     def convert_to_raw_label_scale(self, dst_l_cut, preds):
         if (self.i2cat is None) or (self.scalers_dict is None):
-            raise RuntimeError(
-                "self.i2cat or self.scalers_dict is None. Run ScaleUtil.transform_df function first")
+            raise RuntimeError("self.i2cat or self.scalers_dict is None. Run ScaleUtil.transform_df function first")
         raw_preds = []
         for dst, pred in zip(dst_l_cut, preds):
             cat = self.i2cat[dst]
@@ -218,50 +214,3 @@ class ScaleUtil:
             return np.array(raw_preds)
         else:
             return torch.tensor(raw_preds, dtype=float, device=self.device)
-
-
-class KBinsDiscretizer:
-    def __init__(self, n_bins, strategy="kmeans"):
-        self.n_bins = n_bins
-        self.strategy = strategy
-
-    def fit(self, X):
-        X = X.flatten()
-        # if strategy is kmeans, generte bin_edges_ with sklearn kmeans
-        if self.strategy == "kmeans":
-            from sklearn.cluster import KMeans
-            kmeans = KMeans(n_clusters=self.n_bins,
-                            random_state=0).fit(X.reshape(-1, 1))
-            self.bin_edges_ = np.sort(kmeans.cluster_centers_.flatten())
-            # save the standard deviation of each cluster into self.std
-            self.std_ = np.zeros(self.n_bins)
-            for i in range(self.n_bins):
-                self.std_[i] = np.std(X[kmeans.labels_ == i])
-        else:
-            self.bin_edges_ = np.percentile(
-                X, np.linspace(0, 100, self.n_bins - 1))
-        return self
-
-    def transform(self, X):
-        X = X.flatten()
-        # Assign each value to a bin
-        # if strategy is kmeans, assign X_binned to the closest bin_edges_
-        if self.strategy == "kmeans":
-            X_binned = np.zeros(len(X), dtype=int)
-            for i in range(len(X)):
-                X_binned[i] = np.argmin(np.abs(X[i] - self.bin_edges_))
-        else:
-            X_binned = np.digitize(X, self.bin_edges_, right=True) - 1
-        # Create one-hot encoding
-        one_hot = np.zeros((len(X), self.n_bins))
-        one_hot[np.arange(len(X)), X_binned] = 1
-        return one_hot
-
-    def fit_transform(self, X):
-        return self.fit(X).transform(X)
-
-    def inverse_transform(self, X):
-        return self.bin_edges_[X - 1]
-
-    def __repr__(self):
-        return "KBinsDiscretizer(n_bins={})".format(self.n_bins)
--- learn_edge.py
+++ learn_edge.py
@@ -8,7 +8,7 @@ import argparse
 
 import torch
 import numpy as np
-# import numba
+#import numba
 from tqdm import tqdm
 
 from sklearn.metrics import r2_score
@@ -21,11 +21,9 @@ from graph import NeighborFinder
 from utils import EarlyStopMonitor, RandEdgeSampler
 import os
 
-# Argument and global variables
-parser = argparse.ArgumentParser(
-    'Interface for TGAT experiments on link predictions')
-parser.add_argument('-d', '--data', type=str,
-                    help='data sources to use, try wikipedia or reddit', default='u2k_i200_1W')
+### Argument and global variables
+parser = argparse.ArgumentParser('Interface for TGAT experiments on link predictions')
+parser.add_argument('-d', '--data', type=str, help='data sources to use, try wikipedia or reddit', default='u2k_i200_1W')
 parser.add_argument('--bs', type=int, default=200, help='batch_size')
 parser.add_argument('--prefix', type=str, default='',
                     help='prefix to name the checkpoints')
@@ -91,13 +89,9 @@ SCALE = args.scale_label
 MODEL_NAME = f'{args.prefix}-{args.agg_method}-{args.attn_mode}-{args.data}'
 MODEL_SAVE_PATH = f'./saved_models/{MODEL_NAME}.pth'
 BEST_METRICS_PATH = f'./best_metrics/{args.data}-best-metrics.txt'
+get_checkpoint_path = lambda epoch: f'./saved_checkpoints/{MODEL_NAME}-{epoch}.pth'
 
-
-def get_checkpoint_path(
-    epoch): return f'./saved_checkpoints/{MODEL_NAME}-{epoch}.pth'
-
-
-# set up logger
+### set up logger
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -105,18 +99,12 @@ fh = logging.FileHandler('log/{}.log'.format(str(time.time())))
 fh.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.WARN)
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
 ch.setFormatter(formatter)
 logger.addHandler(fh)
 logger.addHandler(ch)
 logger.info(args)
-
-# if GPU >= 0:
-# device = torch.device('cuda:{}'.format(GPU))
-# else:
-
 
 if GPU >= 0:
     device = torch.device('cuda:{}'.format(GPU))
@@ -130,10 +118,8 @@ node_features, edge_features, full_data, train_data, val_data, test_data, scaleU
 
 
 # write a function to store the best metrics and the corresponding model checkpoint
-
-
 def store_checkpoint(epoch, tgan, optimizer):
-    latest_path = get_checkpoint_path('latest')
+    latest_path  = get_checkpoint_path('latest')
     torch.save({
         'epoch': epoch,
         'model_state_dict': tgan.state_dict(),
@@ -142,12 +128,11 @@ def store_checkpoint(epoch, tgan, optimizer):
     logger.info('Checkpoint saved to {}'.format(latest_path))
     return
 
-
 def eval_one_epoch(tgan, sampler, src, dst, ts, label, label_raw):
     val_mae_raw, val_mae, val_r2_raw, val_r2 = [], [], [], []
     with torch.no_grad():
         tgan = tgan.eval()
-        TEST_BATCH_SIZE = 30
+        TEST_BATCH_SIZE=30
         num_test_instance = len(src)
         num_test_batch = math.ceil(num_test_instance / TEST_BATCH_SIZE)
         for k in range(num_test_batch):
@@ -191,18 +176,15 @@ def eval_one_epoch(tgan, sampler, src, dst, ts, label, label_raw):
             val_mae.append(mean_absolute_error(true_label, pred_score))
             val_r2.append(r2_score(true_label, pred_score))
             if args.scale_label != 'none':
-                pred_score_raw = scaleUtil.convert_to_raw_label_scale(
-                    np.concatenate([dst_l_cut, dst_l_fake]), pred_score)
+                pred_score_raw = scaleUtil.convert_to_raw_label_scale(np.concatenate([dst_l_cut,dst_l_fake]), pred_score)
                 val_r2_raw.append(r2_score(true_label_raw, pred_score_raw))
-                val_mae_raw.append(mean_absolute_error(
-                    true_label_raw, pred_score_raw))
+                val_mae_raw.append(mean_absolute_error(true_label_raw, pred_score_raw))
 
     return np.mean(val_mae_raw), np.mean(val_mae), np.mean(val_r2_raw), np.mean(val_r2)
 
-
 max_src_index = full_data.sources.max()
 max_idx = max(full_data.sources.max(), full_data.destinations.max())
-# Initialize the data structure for graph and edge sampling
+### Initialize the data structure for graph and edge sampling
 # build the graph for fast query
 # graph only contains the training data (with 10% nodes removal)
 adj_list = [[] for _ in range(max_idx + 1)]
@@ -218,20 +200,18 @@ for src, dst, eidx, ts in zip(full_data.sources, full_data.destinations, full_da
     full_adj_list[dst].append((src, eidx, ts))
 full_ngh_finder = NeighborFinder(full_adj_list, uniform=UNIFORM)
 
-train_rand_sampler = RandEdgeSampler(
-    train_data.sources, train_data.destinations)
+train_rand_sampler = RandEdgeSampler(train_data.sources, train_data.destinations)
 val_rand_sampler = RandEdgeSampler(full_data.sources, full_data.destinations)
 # nn_val_rand_sampler = RandEdgeSampler(nn_val_data.sources, nn_val_data.destinations)
 test_rand_sampler = RandEdgeSampler(full_data.sources, full_data.destinations)
 # nn_test_rand_sampler = RandEdgeSampler(nn_test_src_l, nn_test_data.destinations)
 
 
-# Model initialize
+### Model initialize
 tgan = TGAN(train_ngh_finder, node_features, edge_features,
             num_layers=NUM_LAYER, num_classes=NUM_CLASSES, classification_model=True, use_time=USE_TIME, agg_method=AGG_METHOD,
             attn_mode=ATTN_MODE,
             seq_len=SEQ_LEN, n_head=NUM_HEADS, drop_out=DROP_OUT, node_dim=NODE_DIM, time_dim=TIME_DIM)
-
 optimizer = torch.optim.Adam(tgan.parameters(), lr=LEARNING_RATE)
 if args.loss == 'l1':
     criterion = torch.nn.L1Loss()
@@ -257,7 +237,6 @@ for epoch in range(NUM_EPOCH):
     mae_raw, mae, r2_raw, r2, m_loss = [], [], [], [], []
     np.random.shuffle(idx_list)
     logger.info('start {} epoch'.format(epoch))
-
     for k in tqdm(range(num_batch)):
         # percent = 100 * k / num_batch
         # if k % int(0.2 * num_batch) == 0:
@@ -265,8 +244,7 @@ for epoch in range(NUM_EPOCH):
 
         s_idx = k * BATCH_SIZE
         e_idx = min(num_instance - 1, s_idx + BATCH_SIZE)
-        src_l_cut, dst_l_cut = train_data.sources[s_idx:
-                                                  e_idx], train_data.destinations[s_idx:e_idx]
+        src_l_cut, dst_l_cut = train_data.sources[s_idx:e_idx], train_data.destinations[s_idx:e_idx]
         ts_l_cut = train_data.timestamps[s_idx:e_idx]
         label_l_cut = train_data.labels[s_idx:e_idx]
         raw_label_l_cut = train_data.raw_labels[s_idx:e_idx]
@@ -281,13 +259,10 @@ for epoch in range(NUM_EPOCH):
 
         optimizer.zero_grad()
         tgan = tgan.train()
-        pos_pred, neg_pred = tgan.contrast(
-            src_l_cut, dst_l_cut, dst_l_fake, ts_l_cut, NUM_NEIGHBORS)
+        pos_pred, neg_pred = tgan.contrast(src_l_cut, dst_l_cut, dst_l_fake, ts_l_cut, NUM_NEIGHBORS)
 
-        print(pos_pred.shape, neg_pred.shape, pos_label.shape, neg_label.shape)
         # if args.scale_label == 'none':
-        loss = criterion(torch.concat(
-            [pos_pred, neg_pred]), torch.concat([pos_label, neg_label]))
+        loss = criterion(torch.concat([pos_pred, neg_pred]), torch.concat([pos_label, neg_label]))
         # else:
         #     pos_pred_raw, pos_label_raw = convert_to_raw_label_scale(dst_l_cut, pos_pred, pos_label)
         #     neg_pred_raw, neg_label_raw = convert_to_raw_label_scale(dst_l_fake, neg_pred, neg_label)
@@ -322,21 +297,19 @@ for epoch in range(NUM_EPOCH):
 
                 mae.append(mean_absolute_error(true_label, pred_score))
 
-                # f1.append(f1_score(true_label, pred_label))
-                m_loss.append(loss.item())
-                r2.append(r2_score(true_label, pred_score))
-                if args.scale_label != 'none':
-                    pred_score_raw = scaleUtil.convert_to_raw_label_scale(
-                        np.concatenate([dst_l_cut, dst_l_fake]), pred_score)
-                    mae_raw.append(mean_absolute_error(
-                        true_label_raw, pred_score_raw))
-                    r2_raw.append(r2_score(true_label_raw, pred_score_raw))
+            # f1.append(f1_score(true_label, pred_label))
+            m_loss.append(loss.item())
+            r2.append(r2_score(true_label, pred_score))
+            if args.scale_label != 'none':
+                pred_score_raw = scaleUtil.convert_to_raw_label_scale(np.concatenate([dst_l_cut,dst_l_fake]), pred_score)
+                mae_raw.append(mean_absolute_error(true_label_raw, pred_score_raw))
+                r2_raw.append(r2_score(true_label_raw, pred_score_raw))
 
-        # validation phase use all information
+    # validation phase use all information
     tgan.ngh_finder = full_ngh_finder
     val_mae_raw, val_mae, val_r2_raw, val_r2 = eval_one_epoch(tgan, val_rand_sampler, val_data.sources,
-                                                              val_data.destinations, val_data.timestamps,
-                                                              val_data.labels, val_data.raw_labels)
+    val_data.destinations, val_data.timestamps, val_data.labels, val_data.raw_labels)
+
 
     logger.info('epoch: {}:'.format(epoch))
     logger.info('Epoch mean loss: {:.4f}'.format(np.mean(m_loss)))
@@ -346,44 +319,36 @@ for epoch in range(NUM_EPOCH):
     # logger.info('train f1: {}, val f1: {}, new node val f1: {}'.format(np.mean(f1), val_f1, nn_val_f1))
 
     logger.info('train R2: {:.4f}, val R2: {:.4f}'.format(np.mean(r2), val_r2))
-    logger.info('train MAE: {:.4f}, val MAE: {:.4f}'.format(
-        np.mean(mae), val_mae))
+    logger.info('train MAE: {:.4f}, val MAE: {:.4f}'.format(np.mean(mae), val_mae))
     if args.scale_label != 'none':
-        logger.info('train raw R2: {:.4f}, val raw R2: {:.4f}'.format(
-            np.mean(r2_raw), val_r2_raw))
-        logger.info('train raw MAE: {:.4f}, val raw MAE: {:.4f}'.format(
-            np.mean(mae_raw), val_mae_raw))
+        logger.info('train raw R2: {:.4f}, val raw R2: {:.4f}'.format(np.mean(r2_raw), val_r2_raw))
+        logger.info('train raw MAE: {:.4f}, val raw MAE: {:.4f}'.format(np.mean(mae_raw), val_mae_raw))
 
     if early_stopper.early_stop_check(val_mae):
-        logger.info('No improvment over {} epochs, stop training'.format(
-            early_stopper.max_round))
+        logger.info('No improvment over {} epochs, stop training'.format(early_stopper.max_round))
         break
     else:
         if early_stopper.best_epoch == epoch:
-            logger.info('Best epoch {} is the same as current epoch {}, save the model'.format(
-                early_stopper.best_epoch, epoch))
+            logger.info('Best epoch {} is the same as current epoch {}, save the model'.format(early_stopper.best_epoch, epoch))
             best_model_path = get_checkpoint_path(epoch)
             torch.save(tgan.state_dict(), best_model_path)
     store_checkpoint(epoch, tgan, optimizer)
 
 # Load best model for testing
-logger.info(
-    f'Loading the best model at epoch {early_stopper.best_epoch} for Testing')
+logger.info(f'Loading the best model at epoch {early_stopper.best_epoch} for Testing')
 best_model_path = get_checkpoint_path(early_stopper.best_epoch)
 tgan.load_state_dict(torch.load(best_model_path))
 logger.info(f'Loaded the best model from {best_model_path} for Testing')
 # testing phase use all information
 tgan.ngh_finder = full_ngh_finder
 test_mae_raw, test_mae, test_r2_raw, test_r2 = eval_one_epoch(tgan, test_rand_sampler, test_data.sources,
-                                                              test_data.destinations, test_data.timestamps,
-                                                              test_data.labels, test_data.raw_labels)
+test_data.destinations, test_data.timestamps, test_data.labels, test_data.raw_labels)
 
 logger.info('Test R2: {:.4f}'.format(test_r2))
 logger.info('Test MAE: {:.4f}'.format(test_mae))
 if args.scale_label != 'none':
     logger.info('Test raw R2: {:.4f}'.format(test_r2_raw))
     logger.info('Test raw MAE: {:.4f}'.format(test_mae_raw))
-
 
 def store_best_metrics(mae_raw, mae, r2_raw, r2):
     # check for the existence of the best metric file, if not, create a new file and store the best metrics
@@ -394,17 +359,24 @@ def store_best_metrics(mae_raw, mae, r2_raw, r2):
         line_split = lines[1].split('\t')
         mae_raw_best = float(line_split[2])
         if mae_raw_best < mae_raw:
-            logger.info(
-                f'Existing raw MAE {mae_raw_best} in {BEST_METRICS_PATH} is better than new one {mae_raw}, not storing the new one')
+            logger.info(f'Existing raw MAE {mae_raw_best} in {BEST_METRICS_PATH} is better than new one {mae_raw}, not storing the new one')
             return
     with open(BEST_METRICS_PATH, 'w') as f:
         f.write('model_path\tmae\tmae_raw\tr2\tr2_raw\n')
-        f.write('{}\t{}\t{}\t{}\t{}\n'.format(
-            MODEL_SAVE_PATH, mae, mae_raw, r2, r2_raw))
+        f.write('{}\t{}\t{}\t{}\t{}\n'.format(MODEL_SAVE_PATH, mae, mae_raw, r2, r2_raw))
         f.close()
     logger.info('Saving model')
     torch.save(tgan.state_dict(), MODEL_SAVE_PATH)
     logger.info('Model saved')
 
-
 store_best_metrics(test_mae_raw, test_mae, test_r2_raw, test_r2)
+
+
+
+
+
+
+
+
+
+
